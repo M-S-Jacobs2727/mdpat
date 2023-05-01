d647eb83465f70571d380e8efc6e926306db4f53
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-05-01 19:26:50 -0400

    improve build and tests

--- test/testPermuteDims.cpp
+++ test/testPermuteDims.cpp
@@ -98,7 +98,7 @@ BOOST_AUTO_TEST_CASE(two_proc_2d)
 
     // Before:
     // Proc 0: [ [ 0,  1,  2,  3],
-    //           [ 4,  5,  6,  7] 
+    //           [ 4,  5,  6,  7] ]
     // Proc 1: [ [ 8,  9, 10, 11],
     //           [12, 13, 14, 15] ]
 
@@ -106,7 +106,7 @@ BOOST_AUTO_TEST_CASE(two_proc_2d)
     // Proc 0: [ [ 0,  4,  8, 12],
     //           [ 1,  5,  9, 13] ]
     // Proc 1: [ [ 2,  6, 10, 14],
-    //           [ 3,  7, 11, 15]]
+    //           [ 3,  7, 11, 15] ]
 
     auto ret = MDPAT::permuteDimsParallel<int>(vec, dimLengths, newDims, ME, NPROCS, MPI_COMM_WORLD);
     BOOST_TEST(ret == 0);
--- test/testReadInput.cpp
+++ test/testReadInput.cpp
@@ -1,3 +1,9 @@
-#include <iostream>
-
+#define BOOST_TEST_MODULE header-only testReadInput
+#include <boost/test/included/unit_test.hpp> 
 #include "../src/readInput.hpp"
+// #include <iostream>
+
+BOOST_AUTO_TEST_CASE(test1)
+{
+    BOOST_TEST(1 == 1);
+}
