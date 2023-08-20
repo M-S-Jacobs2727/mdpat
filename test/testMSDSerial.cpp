9965757994d9b980fe8db7e63db6c608162618ed
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-08-20 18:01:49 -0400

    add MPISetup to TestMSDSerial

--- test/testMSDSerial.cpp
+++ test/testMSDSerial.cpp
@@ -6,13 +6,30 @@
 
 namespace fs = std::filesystem;
 
+int ME = 0, NPROCS = 1;
+struct MPISetup
+{
+    MPISetup()
+    {
+        int argc = 0;
+        char **argv = nullptr;
+        MPI_Init(&argc, &argv);
+        MPI_Comm_rank(MPI_COMM_WORLD, &ME);
+        MPI_Comm_size(MPI_COMM_WORLD, &NPROCS);
+    }
+    ~MPISetup() { MPI_Finalize(); }
+};
+
+BOOST_TEST_GLOBAL_FIXTURE(MPISetup);
+
+
 BOOST_AUTO_TEST_CASE(msd_serial_1)
 {
     fs::path outfile("msd_test.txt");
     fs::path directory("../test/testFiles");
     MDPAT::StepRange stepRange(0UL, 250UL, 50UL);
     double timestep = 0.005;
-    uint32_t atomType = 1;
+    uint32_t atomType = 0;
     uint64_t minGap = 0UL;
     uint64_t maxGap = 100UL;
     uint32_t dim = 3;
