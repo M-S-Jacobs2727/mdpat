122dcc8c78aae62ae1118b945d3dccd52d067306
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-08-20 18:00:46 -0400

    make splitValues header-only

--- src/splitValues.hpp
+++ src/splitValues.hpp
@@ -1,10 +1,28 @@
 #pragma once
 
-#include <cstddef>
 #include <cstdint>
+#include <cstdlib>
 #include <utility>
 
 namespace MDPAT
 {
-    std::pair<uint64_t, uint64_t> splitValues(uint64_t totalNumValues, int me, int nProcs);
+    /*
+     * Returns the first index and number of values for process me when split evenly
+     * among nprocs processes.
+     */
+    std::pair<uint64_t, uint64_t> splitValues(uint64_t totalNumValues, int me, int nProcs)
+    {
+        std::pair<uint64_t, uint64_t> pair;
+        const auto div = std::div(totalNumValues, nProcs);
+
+        pair.first = me * (div.quot + 1);
+        pair.second = div.quot + 1;
+        
+        if (me >= div.rem)
+        {
+            pair.first += div.rem - me;
+            pair.second -= 1;
+        }
+        return pair;
+    }
 }
