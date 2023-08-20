3b127616bef98506503a3f54ddb3f5d0ef71a103
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-08-20 18:07:09 -0400

    change build system to premake

---
+++ premake5.lua
@@ -0,0 +1,26 @@
+-- Premake test
+
+workspace "MDPAT"
+    configurations { "debug", "release" }
+
+project "MDPAT"
+    kind "ConsoleApp"
+    language "C++"
+    location "build"
+    links { "mpi" }
+    libdirs { os.findlib("mpi", "${HOME}/.local") }
+
+    files { "src/*.hpp", "src/*.cpp" }
+
+    includedirs { "${HOME}/.local/include" }
+
+    filter "action:gmake2"
+        buildoptions {"-std=c++17"}
+
+    filter "configurations:debug"
+        defines {"DEBUG"}
+        symbols "On"
+
+    filter "configurations:release"
+        defines {"NDEBUG"}
+        optimize "Speed"
